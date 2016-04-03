aab9c75738be13477ace4ba49c247af66ad052c4
stephenmeszaros <contact.meszaros@gmail.com>
2016-04-03 18:03:03 -0400

    Lotta updates

--- config.rb
+++ config.rb
@@ -1,15 +1,15 @@
-###
+####################
 # Compass
-###
+####################
 
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
 # end
 
-###
+####################
 # Page options, layouts, aliases and proxies
-###
+####################
 
 # Per-page layout changes:
 #
@@ -28,9 +28,9 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-###
+####################
 # Helpers
-###
+####################
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
@@ -53,16 +53,31 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+####################
+# Deploy-specific configuration
+####################
+
+activate :deploy do |deploy|
+  deploy.deploy_method   = :ftp
+  deploy.host            = "ftp.stephenmeszaros.com"
+  deploy.path            = "/almanac.design"
+  deploy.user            = "admin@stephenmeszaros.com"
+  deploy.password        = ")hEv=gsP^%ttkmkff>3k8294Q4sa7g.RzdJ8;"
+end
+
+####################
 # Build-specific configuration
+####################
+
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
